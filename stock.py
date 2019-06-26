43ccb76930c279e6fdbce09016b23ade809ff98c
Angrybird <shrestha_dss@hotmail.com>
2019-06-27 04:08:12 +0545

    phase 1 completed

--- stock.py
+++ stock.py
@@ -1,14 +1,13 @@
 
+# Part 1 - Data Preprocessing
 
-#1. Data  preprocessing
-
-#importing dependencies
+# Importing the libraries
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as mplt
 
 #importing the training set
-dataset_train = pd.read_csv("Google_Stock_Price_Train.csv")
+dataset_train = pd.read_csv('Google_Stock_Price_Train.csv')
 training_set = dataset_train.iloc[:, 1:2].values 
 
 #feature scaling
@@ -23,9 +22,11 @@ training_set_scaled = sc.fit_transform(training_set)
 #observe the data from previous 60 timestamps/features (variable) to make future predictions
 X_train = []
 y_train = []
-for i in range(60, len(training_set_scaled)):
+for i in range(60, 1258):
     X_train.append(training_set_scaled[i-60:i, 0])
     y_train.append(training_set_scaled[i, 0])
+
+# 1258 = len(training_set_scaled)
 X_train, y_train = np.array(X_train), np.array(y_train)
 
 #Reshaping
@@ -33,42 +34,85 @@ X_train, y_train = np.array(X_train), np.array(y_train)
 X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1],1))
 
 
+
 #2. Building the RNN
 
 #importing Keras libraries and packages
-import tensorflow as tf
-from Keras.models import Sequential
-from Keras.layers import Dense
-from Keras.layers import LSTM
-from Keras.layers import Dropout
+from keras.models import Sequential
+from keras.layers import Dense #to create neurons
+from keras.layers import LSTM
+from keras.layers import Dropout
+#Dropout helps to deactivate the neurons that give '0' output in each iteration/epochs
+#Dense: used only for output
 
-#initializing RNN
+# Initialising the RNN
 regressor = Sequential()
 
-#adding thr first LSTM layer and show dropout regularization
-regressor.add(LSTM(units =50, return_sequences = True, input_shape = (X_train.shape[1], 1)))
+# Adding the first LSTM layer and some Dropout regularisation
+regressor.add(LSTM(units = 50, return_sequences = True, input_shape = (X_train.shape[1], 1)))
+regressor.add(Dropout(0.2))
+#dropout ratio of 20 percent
+
+# Adding a second LSTM layer and some Dropout regularisation
+#hidden layer
+regressor.add(LSTM(units = 50, return_sequences = True))
+regressor.add(Dropout(0.2))
+#units=50-->neurons
+
+# Adding a third LSTM layer and some Dropout regularisation
+#hidden layer
+regressor.add(LSTM(units = 50, return_sequences = True))
 regressor.add(Dropout(0.2))
 
-#adding the second LSTM layer and show Dropout regularization
-regressor.add(LSTM(units =50, return_sequences = True))
+# Adding a fourth LSTM layer and some Dropout regularisation
+#hidden layer
+regressor.add(LSTM(units = 50))
 regressor.add(Dropout(0.2))
 
-#adding the third LSTM layer and show Dropout regularization
-regressor.add(LSTM(units =50, return_sequences = True))
-regressor.add(Dropout(0.2))
+# Adding the output layer
+#Dense layer
+regressor.add(Dense(units = 1))
 
-#adding the fourth LSTM layer and show dropout regularization
-regressor.add(LSTM(units =50, return_sequences = True))
-regressor.add(Dropout(0.2))
-
-#adding the output layer
-regressor.add(Dense(units = 2))
-
-#compiling RNN
-regressor.compile(optimizer = 'adam', loss = 'mean_squared_erroe')
-
-#fitting RNN into the training set
-regressor.fit(X_train, y_train, epochs = 100, batch_size = 32)
+# Compiling the RNN
+regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
+#find the global minima point very quickly
 
 
-#3. 
+# Fitting the RNN to the Training set
+#fit-->run epochs
+regressor.fit(X_train, y_train, epochs = 10, batch_size = 32)
+
+
+
+
+#3.Making the predictions and visualizing  the results
+
+#getting the real stock price of 2017
+dataset_test = pd.read_csv('Google_Stock_Price_Test.csv')
+real_stock_price = dataset_test.iloc[:, 1:2].values
+
+#getting the predicated stock price of 2017
+dataset_total = pd.concat((dataset_train['Open'], dataset_test['Open']), axis = 0)
+inputs = dataset_total[len(dataset_total) - len(dataset_test) -60:].values
+#take previous 60 data
+inputs = inputs.reshape(-1, 1)
+inputs = sc.transform(inputs)
+X_test =[]
+for i in range(60, 80):
+    X_test.append(inputs[i-60:i, 0])
+
+X_test = np.array(X_test)
+X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+predicted_stock_price = regressor.predict(X_test)
+predicted_stock_price = sc.inverse_transform(predicted_stock_price)
+
+#gets inversely transformed to the value that was initially defined
+
+#visulaizing the results
+mplt.plot(real_stock_price, color ='red', label = 'Real Google Stock Price')
+mplt.plot(predicted_stock_price, color = 'blue', label = 'Predicted Google Stock Price')
+mplt.title('Google Stock Price Prediction')
+mplt.xlabel('Time')
+mplt.ylabel('Google Stock Price')
+mplt.legend()
+mplt.show()
